66a1ac8fe8dcc587cf350905f3c30acc72aba02f
lordszn3 <robertkuzhin@gmail.com>
2022-07-22 19:35:48 +0500

    feat(app): load users when app started

--- src/app/application.tsx
+++ src/app/application.tsx
@@ -1,7 +1,11 @@
+import { sample } from 'effector'
 import { useEffect } from 'react'
 import { Pages } from '~/pages'
+import { loadInfiniteUsersFx } from '~/pages/main/model'
 import { appStarted } from '~/shared/config/run-logic'
 import './global-styles.css'
+
+sample({ clock: appStarted, target: loadInfiniteUsersFx })
 
 export const Application = () => {
   useEffect(() => {
