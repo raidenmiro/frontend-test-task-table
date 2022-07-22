e10bfeba447d33c675d6a100fe9b1fd9972a0b62
lordszn3 <robertkuzhin@gmail.com>
2022-07-22 17:34:46 +0500

    feat(app): add run logic event

--- src/app/application.tsx
+++ src/app/application.tsx
@@ -1,5 +1,11 @@
+import { useEffect } from 'react'
+import { appStarted } from '~/shared/config/run-logic'
 import './global-styles.css'
 
 export const Application = () => {
+  useEffect(() => {
+    appStarted()
+  }, [])
+
   return <>Hello effector!</>
 }
