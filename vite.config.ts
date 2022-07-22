cae4b405460130e697a3a6824314261c0822bd79
lordszn3 <robertkuzhin@gmail.com>
2022-07-22 16:27:47 +0500

    chore: add babel

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import * as path from 'path'
+import babel from '@rollup/plugin-babel'
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 
 // eslint-disable-next-line import/no-anonymous-default-export
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), babel({ extensions: ['.ts'] })],
   resolve: {
     alias: {
       '~': path.resolve(__dirname, 'src'),
