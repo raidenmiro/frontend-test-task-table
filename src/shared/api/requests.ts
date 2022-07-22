796a8c40b8bb9d14ec981675a8318e5f3b73dfdb
lordszn3 <robertkuzhin@gmail.com>
2022-07-22 19:22:42 +0500

    feat(shared/api): add requests

--- src/shared/api/requests.ts
+++ src/shared/api/requests.ts
@@ -1,3 +1,45 @@
 import { createEffect } from 'effector'
+import { request } from './config'
+import { User } from './types'
 
-export const loadUsersFx = createEffect()
+export const loadUsersFx = createEffect<{ page: number }, User[]>(
+  async ({ page }) => {
+    const users = await request<User[]>({
+      path: 'users',
+      query: { page: page.toString() },
+    })
+    return users
+  }
+)
+
+export interface UserBody {
+  body: Omit<User, 'id' | 'created_at'>
+}
+
+export const saveNewUserFx = createEffect<UserBody, { user: User }>(
+  async ({ body }) => {
+    const user = await request<{ user: User }>({
+      path: 'users',
+      method: 'POST',
+      body,
+    })
+
+    return user
+  }
+)
+
+export interface RequestParams {
+  userId: string
+}
+
+export const deleteUserFx = createEffect<RequestParams, { message: string }>(
+  async ({ userId }) => {
+    const answer = await request<{ message: string }>({
+      path: 'users',
+      method: 'DELETE',
+      query: { id: userId },
+    })
+
+    return answer
+  }
+)
