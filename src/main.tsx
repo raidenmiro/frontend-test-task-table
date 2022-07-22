72a242ed53bc111ee014e7cb16fccb55ec6ef7ef
lordszn3 <robertkuzhin@gmail.com>
2022-07-22 20:30:47 +0500

    feat: add run mock server

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,10 @@
 import ReactDOM from 'react-dom/client'
 import { Application } from './app/application'
+import { runMockServer } from './shared/api/mock/server'
 
 const root = document.querySelector('#root')
+
+runMockServer()
 
 if (root) {
   ReactDOM.createRoot(root).render(<Application />)
