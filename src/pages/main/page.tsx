7b2ca92902faa0da15290b11a4303ff9a90535ed
lordszn3 <robertkuzhin@gmail.com>
2022-07-22 20:16:02 +0500

    feat(pages): add content with spinner

--- src/pages/main/page.tsx
+++ src/pages/main/page.tsx
@@ -1,3 +1,23 @@
+import { splitView } from '~/shared/lib/variant-view'
+import { Spinner } from '~/shared/ui/spinner'
+import { $users } from './model'
+
+const Content = splitView({
+  source: $users.map((users) => {
+    if (users.length === 0) return 'notReady'
+
+    return 'ready'
+  }),
+  cases: {
+    notReady: () => <Spinner className="flex justify-center" />,
+    ready: () => <span>Hello effector!!!</span>,
+  },
+})
+
 export const Main = () => {
-  return <></>
+  return (
+    <section className="container mx-auto">
+      <Content />
+    </section>
+  )
 }
