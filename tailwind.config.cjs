248cada87d1728247b5212eeb6c52c64e06aef1f
lordszn3 <robertkuzhin@gmail.com>
2022-07-22 16:22:38 +0500

    chore: add tailwind

---
+++ tailwind.config.cjs
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {
+      keyframes: {
+        'infinite-linear': {
+          '0%': { transform: 'translateX(0) scaleX(0)' },
+          '40%': { transform: 'translateX(0) scaleX(0.4)' },
+          '100%': { transform: 'translateX(100%) scaleX(0.5)' },
+        },
+      },
+      animation: {
+        'infinite-linear': 'infinite-linear 1s infinite linear',
+      },
+    },
+  },
+  plugins: [],
+}
