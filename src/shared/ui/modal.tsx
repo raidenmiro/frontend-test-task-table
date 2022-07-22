566d2cf79cdaeaab0bd63d0a1b83d3412ddf2337
lordszn3 <robertkuzhin@gmail.com>
2022-07-22 17:23:07 +0500

    feat(shared/ui): create base modal

---
+++ src/shared/ui/modal.tsx
@@ -0,0 +1,49 @@
+import { Dialog, Transition } from '@headlessui/react'
+import { Fragment, FunctionComponent, ReactNode } from 'react'
+
+export interface ModalProps {
+  close(): void
+  isOpen: boolean
+  children: ReactNode
+}
+
+export const Modal: FunctionComponent<ModalProps> = ({
+  close,
+  isOpen,
+  children,
+}) => {
+  return (
+    <Transition appear show={isOpen} as={Fragment}>
+      <Dialog as="div" className="relative z-10" onClose={close}>
+        <Transition.Child
+          as={Fragment}
+          enter="transition-opacity ease-linear duration-300"
+          enterFrom="opacity-0"
+          enterTo="opacity-100"
+          leave="transition-opacity ease-linear duration-300"
+          leaveFrom="opacity-100"
+          leaveTo="opacity-0"
+        >
+          <div className="fixed inset-0 bg-black bg-opacity-25" />
+        </Transition.Child>
+        <div className="fixed inset-0 overflow-y-auto">
+          <div className="flex min-h-full items-center justify-center p-4 text-center">
+            <Transition.Child
+              as={Fragment}
+              enter="transition ease-in-out duration-300 transform"
+              enterFrom="-translate-y-full"
+              enterTo="translate-y-0"
+              leave="transition ease-in-out duration-300 transform"
+              leaveFrom="translate-y-0"
+              leaveTo="-translate-y-full"
+            >
+              <Dialog.Panel className="w-full max-w-xl transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all">
+                {children}
+              </Dialog.Panel>
+            </Transition.Child>
+          </div>
+        </div>
+      </Dialog>
+    </Transition>
+  )
+}
