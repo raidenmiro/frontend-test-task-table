8686b4ac453ed22284bae688487b96b4e87720ac
lordszn3 <robertkuzhin@gmail.com>
2022-07-25 13:42:53 +0500

    refactor(shared/lib): replace custom lib on composable-form

--- src/features/add-user/ui/form.tsx
+++ src/features/add-user/ui/form.tsx
@@ -1,3 +1,4 @@
+import { reflect } from '@effector/reflect'
 import { Dialog } from '@headlessui/react'
 import { XIcon } from '@heroicons/react/outline'
 import { useUnit } from 'effector-react'
@@ -9,7 +10,6 @@ import { Modal } from '~/shared/ui/modal'
 import {
   $loading,
   email,
-  userForm,
   formSubmitted,
   fullname,
   iban,
@@ -21,8 +21,6 @@ import { ListboxStatus } from './listbox'
 
 export const CreateUserForm = () => {
   const { isOpen } = modal.useModal()
-  const { form, hasError, errors, touchedFields } = userForm.useForm()
-  const pending = useUnit($loading)
 
   return (
     <Modal isOpen={isOpen} close={modal.hide}>
@@ -35,67 +33,99 @@ export const CreateUserForm = () => {
           <XIcon className="w-5 h-5" />
         </ActionButton>
       </Dialog.Title>
-      <form
-        className="mt-9"
-        onSubmit={(evt) => {
-          evt.preventDefault()
-          formSubmitted()
-        }}
-      >
-        <Input
-          id="fullname"
-          label="fullname"
-          className="mb-5"
-          variant={checkError(errors.fullname, touchedFields.fullname)}
-          helperText={failureMessage(errors.fullname)}
-          placeholder="example: John Due"
-          value={form.fullname}
-          onChange={fullname.onValueChanged}
-          onBlur={fullname.onTouched}
-        />
-        <ListboxStatus
-          className="mb-5"
-          selected={form.status}
-          onChange={(variant) => status.onChanged(variant)}
-        />
-        <Input
-          id="email"
-          label="email"
-          className="mb-5"
-          variant={checkError(errors.email, touchedFields.email)}
-          helperText={failureMessage(errors.email)}
-          placeholder="example johndue@gmail.com"
-          value={form.email}
-          onChange={email.onValueChanged}
-          onBlur={email.onTouched}
-        />
-        <Input
-          id="phone"
-          label="phone"
-          type="tel"
-          className="mb-5"
-          variant={checkError(errors.phone, touchedFields.phone)}
-          helperText={failureMessage(errors.phone)}
-          placeholder="+7 (932) 232 123"
-          value={form.phone}
-          onChange={phone.onValueChanged}
-          onBlur={phone.onTouched}
-        />
-        <Input
-          id="iban"
-          label="iban"
-          className="mb-5"
-          value={form.iban}
-          onChange={iban.onValueChanged}
-          variant={checkError(errors.iban, touchedFields.iban)}
-          helperText={failureMessage(errors.iban)}
-          placeholder="example: DE85 0098 7065 0010 2900 94"
-          onBlur={iban.onTouched}
-        />
-        <Button type="submit" loading={pending} disabled={pending || hasError}>
-          Submit
-        </Button>
-      </form>
+      <Form />
     </Modal>
   )
 }
+
+const Form = () => {
+  const pending = useUnit($loading)
+  return (
+    <form
+      className="mt-9"
+      onSubmit={(evt) => {
+        evt.preventDefault()
+        formSubmitted()
+      }}
+    >
+      <FullnameField />
+      <StatusField />
+      <EmailField />
+      <PhoneField />
+      <IbanField />
+      <Button type="submit" loading={pending} disabled={pending}>
+        Submit
+      </Button>
+    </form>
+  )
+}
+
+const FullnameField = reflect({
+  view: Input,
+  bind: {
+    id: 'fullname',
+    label: 'fullname',
+    className: 'mb-5',
+    placeholder: 'example: John Due',
+    onChange: fullname.onValueChanged,
+    value: fullname.$value,
+    helperText: failureMessage(fullname.$errors),
+    variant: checkError(fullname.$errors, fullname.$isTouched),
+    onBlur: fullname.touched,
+  },
+})
+
+const EmailField = reflect({
+  view: Input,
+  bind: {
+    id: 'email',
+    label: 'email',
+    className: 'mb-5',
+    placeholder: 'example johndue@gmail.com',
+    onChange: email.onValueChanged,
+    value: email.$value,
+    helperText: failureMessage(email.$errors),
+    variant: checkError(email.$errors, email.$isTouched),
+    onBlur: email.touched,
+  },
+})
+
+const StatusField = reflect({
+  view: ListboxStatus,
+  bind: {
+    className: 'mb-5',
+    selected: status.$value,
+    onChange: status.changed,
+  },
+})
+
+const PhoneField = reflect({
+  view: Input,
+  bind: {
+    id: 'phone',
+    label: 'phone',
+    type: 'tel',
+    className: 'mb-5',
+    placeholder: '+7 (932) 232 123',
+    onChange: phone.onValueChanged,
+    value: phone.$value,
+    helperText: failureMessage(phone.$errors),
+    variant: checkError(phone.$errors, phone.$isTouched),
+    onBlur: phone.touched,
+  },
+})
+
+const IbanField = reflect({
+  view: Input,
+  bind: {
+    id: 'iban',
+    label: 'iban',
+    className: 'mb-5',
+    placeholder: 'example: DE85 0098 7065 0010 2900 94',
+    onChange: iban.onValueChanged,
+    value: iban.$value,
+    helperText: failureMessage(iban.$errors),
+    variant: checkError(iban.$errors, iban.$isTouched),
+    onBlur: iban.touched,
+  },
+})
