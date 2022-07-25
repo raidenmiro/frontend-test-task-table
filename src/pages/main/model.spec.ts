128648e8a05587c167c7c0fb7a4af588cc58fa68
lordszn3 <robertkuzhin@gmail.com>
2022-07-25 15:29:39 +0500

    test(pages/main): add test on infinite loading

---
+++ src/pages/main/model.spec.ts
@@ -0,0 +1,33 @@
+import { allSettled, fork } from 'effector'
+import { Server } from 'miragejs'
+import { runMockServer } from '~/shared/api/mock/server'
+import { $page, $users, scrolledToLast } from './model'
+
+let server: Server
+
+beforeAll(() => {
+  server = runMockServer()
+})
+
+afterAll(() => {
+  server.shutdown()
+})
+
+describe('infinite loading', () => {
+  test('load users when scrolled table to the end', async () => {
+    const scope = fork({
+      values: new Map().set($users, []),
+    })
+
+    expect(scope.getState($page)).toBe(0)
+    expect(scope.getState($users)).toHaveLength(0)
+
+    await allSettled(scrolledToLast, {
+      params: true,
+      scope,
+    })
+
+    expect(scope.getState($users)).toHaveLength(20)
+    expect(scope.getState($page)).toBe(1)
+  })
+})
